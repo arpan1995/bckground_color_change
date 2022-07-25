7c125d97f6e62ae94cd67b034e82d42ff860d65f
Arpan Das <97222490+arpan1995@users.noreply.github.com>
2022-07-25 12:31:10 +0530

    Add files via upload

---
+++ mind.js
@@ -0,0 +1,27 @@
+// var buttonUse = document.getElementById('buttonuse');
+// var bodyColor = document.body;
+// var colorp = ['pink','red','white', 'black', 'blue', 'neon']
+
+
+// // buttonUse.addEventListener('click', bodybackgroundcolor)
+
+// // function bodybackgroundcolor(){
+// //     const colorIndex = Math.floor(Math.random()*colorp.length)
+// //     bodyColor.style.backgroundColor = colorp[colorIndex];
+// // }
+
+
+// // buttonUse.addEventListener('click', function bodybackgroundcolor(){
+// //     const colorIndex = Math.floor(Math.random()*colorp.length)
+// //     bodyColor.style.backgroundColor = colorp[colorIndex];
+// // })
+
+
+
+// buttonUse.addEventListener('click', () =>{
+//     const colorIndex = Math.floor(Math.random()*colorp.length)
+//     bodyColor.style.backgroundColor = colorp[colorIndex];
+// })
+
+
+
